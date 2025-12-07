bbbc76d66381255ec686f263c5d22f733195616f
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2025-12-07 15:37:34 +0530

    fixed test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
 from fastapi.testclient import TestClient
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
 
 client = TestClient(app)
